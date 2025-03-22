81278755dd9bb8f92c6847374b605bf480633857
Bruno Mota <bruno7motadev@gmail.com>
2025-03-22 15:32:03 -0300

    feat: Criado User, UserRole, UserRepository, AutorizationService

---
+++ src/main/java/br/com/crud/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.crud.repository;
+
+import br.com.crud.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByEmail(String email);
+}
